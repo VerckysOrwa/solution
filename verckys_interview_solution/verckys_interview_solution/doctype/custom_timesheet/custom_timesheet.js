af5ea5adabdabc11121e5b09ee046daefbad59a8
VerckysOrwa <vrcxabayo@gmail.com>
2024-10-21 20:08:12 +0300

    unchecked the 'custom' field on the doctype settings

---
+++ verckys_interview_solution/verckys_interview_solution/doctype/custom_timesheet/custom_timesheet.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Verckys Orwa and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Custom Timesheet", {
+// 	refresh(frm) {
+
+// 	},
+// });
