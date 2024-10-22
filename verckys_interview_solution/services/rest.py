59673d5e8737244b83e28289607cc4a6d799121e
VerckysOrwa <vrcxabayo@gmail.com>
2024-10-22 09:12:12 +0300

    Created an API endpoint to fetch sales order items Created an API to create work order Created a client script on the custom work order that fetches the sales order items once the  sales order field is triggered
    created functionality to auto-populate the required items child table in the custom  work order form

--- verckys_interview_solution/services/rest.py
+++ verckys_interview_solution/services/rest.py
@@ -14,5 +14,37 @@ def create_user_permission(docName,userName,doctype):
 
 
 @frappe.whitelist()
-def create_work_order(docName,item,qty,bom_no):
-    pass
+def create_work_order(docName,item,qty,bom_no,targetWarehouse):
+    try:
+        work_order_doc = frappe.get_doc({
+            "doctype": "Custom Work Order",
+            "production_item": item,  
+            "qty_to_manufacture": float(qty),
+            "sales_order": docName,
+            "bom_no": bom_no,
+            "fg_warehouse": targetWarehouse
+        })
+        work_order_doc.insert()
+        work_order_doc.save()
+        return work_order_doc
+        
+    except Exception as e:
+        frappe.throw(f"Error creating Work Order: {str(e)}")
+        frappe.log(e)
+        
+        
+        
+@frappe.whitelist()
+def fetch_sales_order_items(parent):
+    get_items=frappe.get_all("Sales Order Item",{"parent":parent},["item_code","qty","rate","amount"])
+    sales_order_items=[]
+    for item in get_items:
+        sales_order_items.append({
+            "item_code":item.item_code,
+            "qty":item.qty,
+            "rate":item.rate,
+            "amount":item.amount
+        })
+    # print(f"This are the itemse\n\n\n\n{get_items}\n\n\n")
+    return sales_order_items
+    
--- verckys_interview_solution/verckys_interview_solution/doctype/custom_work_order/custom_work_order.js
+++ verckys_interview_solution/verckys_interview_solution/doctype/custom_work_order/custom_work_order.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2024, Verckys Orwa and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Custom Work Order", {
-// 	refresh(frm) {
+frappe.ui.form.on("Custom Work Order", {
+  sales_order(frm) {
+    frappe.call({
+      method:
+        "verckys_interview_solution.services.rest.fetch_sales_order_items",
+      args: {
+        parent: frm.doc.sales_order,
+      },
+      callback: (r) => {
+        if (r) {
+          console.log(r.message);
+          let itemsData = r.message;
+          let item_table = frm.doc.required_items;
 
-// 	},
-// });
+          for (let data of itemsData) {
+            for (let row of item_table) {
+              console.log("this row log", row);
+              row.item_code = data.item_code;
+              row.required_qty = data.qty;
+            }
+          }
+          frm.refresh_field("required_items")
+        }
+      },
+    });
+  },
+});
