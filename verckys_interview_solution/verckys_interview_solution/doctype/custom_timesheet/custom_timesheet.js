36f7d9a442705bf66a66b5975e5d5d2233403d25
VerckysOrwa <vrcxabayo@gmail.com>
2024-10-22 15:09:13 +0300

    added some date validation for the in and out-time fields on the Custom Timesheet doctype

--- verckys_interview_solution/verckys_interview_solution/doctype/custom_timesheet/custom_timesheet.js
+++ verckys_interview_solution/verckys_interview_solution/doctype/custom_timesheet/custom_timesheet.js
@@ -22,19 +22,38 @@ frappe.ui.form.on("Custom Timesheet", {
     });
   },
 
-  after_save:(frm)=>{
-    let name=frm.doc.name
-    let user=frappe.session.user
+  after_save: (frm) => {
+    let name = frm.doc.name;
+    let user = frappe.session.user;
     frappe.call({
-        method:"verckys_interview_solution.services.rest.create_user_permission",
-        args:{
-            docName:name,
-            userName:user,
-            doctype:"Custom Timesheet"
-        },
-        callback:(r)=>{
-            
-        }
-    })
-  }
+      method: "verckys_interview_solution.services.rest.create_user_permission",
+      args: {
+        docName: name,
+        userName: user,
+        doctype: "Custom Timesheet",
+      },
+      callback: (r) => {},
+    });
+  },
+
+  validate: (frm) => {
+    const currentDate = new Date();
+    const inTimeDate = new Date(frm.doc.in_time);
+    const outTimeDate = new Date(frm.doc.out_time);
+
+    if (inTimeDate > currentDate) {
+      frm.set_value("in_time", "");
+      frappe.throw("In Time cannot be a future date");
+    }
+
+    if (outTimeDate > currentDate) {
+      frm.set_value("out_time", "");
+      frappe.throw("Out-time cannot be a future date");
+    }
+
+    if (inTimeDate > outTimeDate) {
+      frm.set_value("out_time", "");
+      frappe.throw("Out Time cannot be less than In Time");
+    }
+  },
 });
